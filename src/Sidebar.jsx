64c2065321b433867857d6567bd0fe15b27ba04e
Your Name <you@example.com>
2025-07-24 13:03:02 +0300

    push12

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -23,29 +23,6 @@ const Sidebar = ({ pageTitle, creatorName })=> {
                         <li><a href="/">{ pageTitle }</a></li>
                         <li><a href="generic.html">Generic</a></li>
                         <li><a href="elements.html">Elements</a></li>
-                        <li>
-                            <span className="opener">Submenu</span>
-                            <ul>
-                                <li><a href="#">Lorem Dolor</a></li>
-                                <li><a href="#">Ipsum Adipiscing</a></li>
-                                <li><a href="#">Tempus Magna</a></li>
-                                <li><a href="#">Feugiat Veroeros</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Etiam Dolore</a></li>
-                        <li><a href="#">Adipiscing</a></li>
-                        <li>
-                            <span className="opener">Another Submenu</span>
-                            <ul>
-                                <li><a href="#">Lorem Dolor</a></li>
-                                <li><a href="#">Ipsum Adipiscing</a></li>
-                                <li><a href="#">Tempus Magna</a></li>
-                                <li><a href="#">Feugiat Veroeros</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Maximus Erat</a></li>
-                        <li><a href="#">Sapien Mauris</a></li>
-                        <li><a href="#">Amet Lacinia</a></li>
                     </ul>
                 </nav>
                 {/* <!-- Section --> */}
