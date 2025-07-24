2c073d7e945382a55bfa821871d1ccdb7ba5f498
Your Name <you@example.com>
2025-07-24 10:45:05 +0300

    push7

--- src/App.jsx
+++ src/App.jsx
@@ -2,83 +2,97 @@ import { useState } from 'react'
 import { useQuery, gql } from '@apollo/client';
 import Landing from './Landing';
 import Sidebar from './Sidebar';
-
+import Postpage from './Postpage';
 import './App.css'
 
 
 const GET_DATA = gql`
-query findPage($pageslug: String) {
-  page(where: {slug: $pageslug}) {
-    id
-    slug
-    pageTitle
-    featureSection {
-      ... on FeaturedPostContent {
-        id
-        featuredPostContent {
-          html
-        }
-        featurePostImage {
-          url
-        }
-      }
-    }
-    createdBy {
-      name
-    }
-    serviceSection {
-      ... on PageSection {
-        sectionContents {
-          ... on ServiceCard {
-            id
-            serviceFaIcon
-            serviceItemDescription
-            serviceItemTitle
+  query findPage($homepageSlug: String, $postSlug: String) {
+    homepage(where: {slug: $homepageSlug}) {
+      id
+      slug
+      pageTitle
+      featureSection {
+        ... on FeaturedPostContent {
+          id
+          featuredPostContent {
+            html
+          }
+          featurePostImage {
+            url
           }
         }
-        sectionTitle
       }
-    }
-    featuredBlogPostSection {
-      ... on PageSection {
-        id
-        sectionContents {
-          ... on BlogPostItem {
-            id
-            featuredBlogpostImage {
-              url
+      createdBy {
+        name
+      }
+      serviceSection {
+        ... on PageSection {
+          sectionContents {
+            ... on ServiceCard {
+              id
+              serviceFaIcon
+              serviceItemDescription
+              serviceItemTitle
             }
-            featuredBlogPostTitle
-            featuredPostExcerpt
           }
+          sectionTitle
         }
-        sectionTitle
+      }
+      featuredBlogPostSection {
+        ... on PageSection {
+          id
+          sectionContents {
+            ... on BlogPostItem {
+              id
+              featuredBlogpostImage {
+                url
+              }
+              featuredBlogPostTitle
+              featuredPostExcerpt
+            }
+          }
+          sectionTitle
+        }
+      }
+    }
+    postPage(where: {slug: $postSlug}) {
+      id
+      postTitle
+      slug
+      postPageContent {
+        html
+      }
+      featuredImage {
+        url
       }
     }
   }
-}
 `;
 
 function App() {
   // Use the useQuery hook to fetch data from the HygraphQL endpoint
   const { loading, error, data } = useQuery(GET_DATA, {
-    variables: { pageslug: 'home' }, // Replace 'home' with the actual slug you want to query
+    variables: { homepageSlug: 'home', postSlug: 'generic' }, // Replace 'home' with the actual slug you want to query
   });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  const pageTitle = data.page.pageTitle;// Get the page title from the query result
-  const creatorName = data.page.createdBy.name; // Get the creator's name from the query result
-  const featureSection = data.page.featureSection; // Get the feature section content
-  const featureContent = data.page.featureSection.featuredPostContent.html; // Get the HTML content of the feature section
-  const featureImageUrl = data.page.featureSection.featurePostImage.url; // Get the feature image URL
-  const serviceSection = data.page.serviceSection; // Get the service section content
-  const serviceSectionTitle = data.page.serviceSection.sectionTitle; // Get the service section title
-  const services = data.page.serviceSection.sectionContents; // Get the service items
-  const featuredblogsectiontitle = data.page.featuredBlogPostSection.sectionTitle; // Get the featured blog post section title
-  const featuredBlogPostSection = data.page.featuredBlogPostSection; // Get the featured blog post section content
-  const featuredBlogPosts = data.page.featuredBlogPostSection.sectionContents; // Get the featured blog posts
+  const pageTitle = data.homepage.pageTitle;// Get the page title from the query result
+  const creatorName = data.homepage.createdBy.name; // Get the creator's name from the query result
+  const featureSection = data.homepage.featureSection; // Get the feature section content
+  const featureContent = data.homepage.featureSection.featuredPostContent.html; // Get the HTML content of the feature section
+  const featureImageUrl = data.homepage.featureSection.featurePostImage.url; // Get the feature image URL
+  const serviceSection = data.homepage.serviceSection; // Get the service section content
+  const serviceSectionTitle = data.homepage.serviceSection.sectionTitle; // Get the service section title
+  const services = data.homepage.serviceSection.sectionContents; // Get the service items
+  const featuredblogsectiontitle = data.homepage.featuredBlogPostSection.sectionTitle; // Get the featured blog post section title
+  const featuredBlogPostSection = data.homepage.featuredBlogPostSection; // Get the featured blog post section content
+  const featuredBlogPosts = data.homepage.featuredBlogPostSection.sectionContents; // Get the featured blog posts
+  const postPageTitle = data.postPage.postTitle; // Get the post page title
+  const featuredImage = data.postPage.featuredImage; // Get the featured image for the post page
+  const postPageContent = data.postPage.postPageContent.html; // Get the post page content
 
   // You can now use these variables in your components or pass them as props  
 
@@ -89,6 +103,7 @@ function App() {
           featureContent={featureContent} serviceSection={serviceSection} serviceSectionTitle={serviceSectionTitle} 
           services={services} featuredblogsectiontitle={featuredblogsectiontitle} featuredBlogPosts={featuredBlogPosts} />
           <Sidebar  pageTitle={pageTitle} />
+          {/* <Postpage postPageTitle={postPageTitle} featuredImage={featuredImage} postPageContent={postPageContent} /> */}
       </div>
     </>
   )
---
+++ src/Postpage.jsx
@@ -0,0 +1,61 @@
+import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const Postpage = ({postPageTitle, featuredImage, postPageContent}) => {
+    return (
+    <>
+        {/* Main content */}
+        <div id="main">
+            <div className="inner">
+                {/* <!-- Header --> */}
+                    <header id="header">
+                        <a href="index.html" className="logo"><strong>Editorial</strong> by HTML5 UP</a>
+                        <ul className="icons">
+                            <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+                            <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
+                            <li><a href="#" className="icon brands fa-snapchat-ghost"><span className="label">Snapchat</span></a></li>
+                            <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+                            <li><a href="#" className="icon brands fa-medium-m"><span className="label">Medium</span></a></li>
+                        </ul>
+                    </header>
+
+                {/* <!-- Content --> */}
+                    <section>
+                        <header className="main">
+                            <h1>Generic</h1>
+                        </header>
+
+                        <span className="image main"><img src="images/pic11.jpg" alt="" /></span>
+
+                        <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. className aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+                        <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+
+                        <hr className="major" />
+
+                        <h2>Interdum sed dapibus</h2>
+                        <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. className aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+
+                        <hr className="major" />
+
+                        <h2>Magna etiam veroeros</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. className aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+                        <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+
+                        <hr className="major" />
+
+                        <h2>Lorem aliquam bibendum</h2>
+                        <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. className aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+
+                    </section>
+
+            </div>
+        </div>
+    </>
+    );
+  }
+
+export default Postpage;
