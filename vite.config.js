17e739584507f6ddafdb2ad4b3b2ec1ff70b2b98
Your Name <you@example.com>
2025-07-25 14:58:59 +0300

    push25

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,22 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    mkcert()],
+  plugins: [react(), mkcert()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Separate libraries into their own chunks
+          if (id.includes('node_modules')) {
+            return 'vendor'; // All node_modules will go to vendor.js
+          }
+          
+          // Example: Separate your assets into another chunk
+          if (id.includes('/src/assets/js/')) {
+            return 'assets'; // All assets will go to assets.js
+          }
+        },
+      },
+    },
+  },
 });
